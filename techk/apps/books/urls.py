5c2fdab068202ebf2550cfcf0a4c92aa0dcc19d7
Jorge Guerra <jfguerra00@gmail.com>
2018-07-23 08:21:03 -0400

    Add book apps views and urls

--- techk/apps/books/urls.py
+++ techk/apps/books/urls.py
@@ -7,4 +7,19 @@ urlpatterns = [
         views.BookList.as_view(),
         name='book_list',
     ),
+    url(
+        r'^(?P<book_id>[\d]+)/$',
+        views.BookDetail.as_view(),
+        name='book_detail',
+    ),
+    url(
+        r'^categories/$',
+        views.BookCategoryList.as_view(),
+        name='category_list',
+    ),
+    url(
+        r'^categories/(?P<category_id>[\d]+)/$',
+        views.BooksByCategoryList.as_view(),
+        name='book_by_category_list',
+    ),
 ]
--- techk/apps/books/views.py
+++ techk/apps/books/views.py
@@ -1,8 +1,68 @@
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
 
 from apps.books.models import Book
+from apps.books.models import BookCategory
 
 
-class BookList(ListView):
+class BaseListView(ListView):
+    paginate_by = 20
+
+    def paginate_queryset(self, queryset, page_size):
+        paginator, page, page_object_list, page_has_other_pages = (
+            super(BaseListView, self).paginate_queryset(queryset, page_size)
+        )
+
+        paginator.trimmed_page_range = range(
+            max(page.number - 5, 1),
+            min(page.number + 5, paginator.num_pages + 1)
+        )
+
+        return (paginator, page, page_object_list, page_has_other_pages)
+
+
+class BookList(BaseListView):
     model = Book
-    template_name = 'index.pug'
+    template_name = 'book/list.pug'
+    paginate_by = 20
+
+
+class BookDetail(DetailView):
+    model = Book
+    template_name = 'book/detail.pug'
+    pk_url_kwarg = 'book_id'
+
+
+class BooksByCategoryList(BaseListView):
+    model = Book
+    template_name = 'book/list_by_category.pug'
+    paginate_by = 20
+
+    def get_queryset(self):
+        return Book.objects.filter(
+            category_id=self.kwargs.get('category_id')
+        )
+
+    def get_context_data(self, **kwargs):
+        print("1")
+        context = super(
+            BooksByCategoryList,
+            self
+        ).get_context_data(**kwargs)
+
+        print("1")
+        category = get_object_or_404(
+            BookCategory,
+            pk=self.kwargs.get('category_id'),
+        )
+        print("1")
+
+        context['title'] = category.name
+
+        return context
+
+
+class BookCategoryList(BaseListView):
+    model = BookCategory
+    template_name = 'category/list.pug'
